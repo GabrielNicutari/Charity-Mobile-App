e455b062c2d48dc855194e2c50b774efab8864b4
Gabriel Nicutari <gabriel_nicutari@yahoo.ro>
2021-11-18 20:55:28 +0100

    Updated .gitignore

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.listen(3000, () => {
-  console.log("Listening on port 3000");
+  console.log('Listening on port 3000');
 });
