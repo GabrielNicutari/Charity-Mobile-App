fb1824e5264cd8ad26845d4a44fbee1433ea084f
Cristian Ou <mccrys_00@yahoo.com>
2021-12-03 18:02:12 +0100

    Organisation Schema

--- backend/src/models/Organisation.js
+++ backend/src/models/Organisation.js
@@ -1,1 +1,35 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const organisationSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  bannerImage: {
+    type: String,
+    required: true
+  },
+  motto: {
+    type: String,
+    required: true
+  },
+  keywords: {
+    type: Array,
+    required: true
+  },
+  monthlyGoal: {
+    type: Number,
+    required: true
+  },
+  overview: {
+    type: String,
+    required: true
+  },
+  history: [{ event: String }],
+  achievements: [{ achievement: String }],
+  gallery: [{ picture: String }]
+});
+
+module.exports = mongoose.model('Organisation', organisationSchema);
