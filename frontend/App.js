b65bab0442c2327d7c3e1274f1174c8d5a914501
Gabriel Nicutari <gabriel_nicutari@yahoo.ro>
2021-12-08 17:49:40 +0100

    Backend - Frontend Connection test

--- frontend/App.js
+++ frontend/App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import api from './src/api/server';
 
 export default function App() {
+  useEffect(() => {
+    testConnection();
+  }, []);
+
+  const [data, setData] = useState('');
+
+  const testConnection = async () => {
+    const res = await api.get('/organisations');
+
+    // console.log(data.data.name);
+    setData(res.data[0].name);
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Something {data}</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ frontend/src/api/server.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'http://e041-195-249-188-72.ngrok.io'
+});
