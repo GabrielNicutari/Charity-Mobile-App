96fbbd6e4603f4e6e36ffeffbf80243773e30fcd
Gabriel <gabriel_nicutari@yahoo.ro>
2021-12-21 17:58:56 +0100

    User Model & Auth Route & Middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,16 @@
 require('./models/Organisation');
+require('./models/User');
 const express = require('express');
 const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
 const organisationRoutes = require('./routes/organisationRoutes');
 // const appRouter = require('./router'); // for a better organisation of the routes (more complex apps)
+const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(authRoutes);
 app.use(organisationRoutes);
 
 const mongoUri =
@@ -24,6 +28,11 @@ mongoose.connection.on('connected', () => {
 
 mongoose.connection.on('error', (err) => {
   console.error('Error connecting to mongo', err);
+});
+
+app.get('/', requireAuth, (req, res) => {
+  console.log(req.user);
+  res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(3000, () => {
