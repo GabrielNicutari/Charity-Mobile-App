b4332dfb2e8a6f2c77a2f4c62a23a7d802de2e49
Gabriel <gabriel_nicutari@yahoo.ro>
2021-12-21 18:43:39 +0100

    Push username to User Document

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -6,10 +6,10 @@ const User = mongoose.model('User');
 const router = express.Router();
 
 router.post('/signup', async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password, username } = req.body;
 
   try {
-    const user = new User({ email, password });
+    const user = new User({ email, password, username });
     await user.save();
 
     const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
